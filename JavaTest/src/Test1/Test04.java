686804a6885bd2ca7d5f734db757a83c2e88de06
spirometry <spirometry@naver.com>
2026-05-04 11:04:27 +0900

    add JavaTest study

---
+++ JavaTest/src/Test1/Test04.java
@@ -0,0 +1,33 @@
+package Test1;
+
+import java.util.Scanner;
+
+public class Test04 {
+	public static void main(String[] arge) {
+		
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("점수입력");
+	int score = sc.nextInt();
+        
+	char grade = 0;
+		
+	System.out.println("입력한 점수는 "+ score + " 입니다.");
+	
+
+	if(score >= 90 && score <= 100 ) {
+		grade = 'A';
+	}else if(score >= 80 && score < 90 )  {
+		grade = 'B';
+	}else if(score >= 70 && score < 80 )  {
+		grade = 'C';
+	}else if(score >= 60 && score <70)  {
+		grade = 'D';
+	}else {
+		grade = 'F';
+	}
+	
+    System.out.printf("등급은 %입니다.", grade);
+	
+	}
+}
---
+++ JavaTest/src/Test1/Test09.java
@@ -0,0 +1,27 @@
+package Test1;
+
+public class Test09 {
+	public static void main(String[] args) {
+
+		int count = 0;
+		
+		for(int i = 1 ; i<=9 ; i++) {
+	
+			if(i <=5) {
+			
+			}eles {
+			
+			}
+			
+			for(int j=1 ; j <= 5 - count ; j++){
+				System.out.print(" ");
+			}
+
+			for(int k=1 ; k <= 2 * count - 1 ; k++){
+				System.out.print("*");
+			}
+			
+			System.out.print("\n");
+		}
+    }
+}
