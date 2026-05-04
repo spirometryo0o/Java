1dfff6e57c5c66defb7f55b2be8d73bdb5159c16
spirometry <spirometry@naver.com>
2026-05-04 11:01:11 +0900

    add Ch02 study

---
+++ Ch02/src/sub1/VariableTest.java
@@ -0,0 +1,19 @@
+package sub1;
+
+
+public class VariableTest {
+
+	
+	
+	public static void main(String[] args) {
+
+		int a = 3;
+		int b = 5;
+		
+		System.out.println("a : " + a);
+		
+		
+	}
+	
+	
+}
